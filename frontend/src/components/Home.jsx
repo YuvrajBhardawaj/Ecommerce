8e2575a38010cc6b92aa5af6b08081504a0c728e
YuvrajBhardawaj <126788904+YuvrajBhardawaj@users.noreply.github.com>
2024-10-04 22:09:49 +0530

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -8,6 +8,7 @@ function Home() {
     useEffect(() => {
         axios.get('https://ecommerce-backend-sodu.onrender.com/api/products')
             .then(res => {
+                console.log(res.data);
                 setItems(res.data.data);
             })
             .catch(error => console.error('Error fetching data:', error));
