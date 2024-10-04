2e453522a10825f16c26e2422cd355214470919e
YuvrajBhardawaj <126788904+YuvrajBhardawaj@users.noreply.github.com>
2024-10-04 22:28:58 +0530

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css'; // Ensure this includes the same styles used in Cart and Wishlist
-import axios from 'axios';
+import axios from 'axios'
 function Home() {
     const [items, setItems] = useState([]);
 
     useEffect(() => {
-        axios.get('https://ecommerce-backend-sodu.onrender.com/api/products')
+        fetch('https://ecommerce-backend-sodu.onrender.com/api/products')
             .then(res => res.json())
             .then(data => {
                 setItems(data.data);
