ac07933fae61967191035b808aa3078d7aaf8ed4
YuvrajBhardawaj <126788904+YuvrajBhardawaj@users.noreply.github.com>
2024-10-05 11:04:27 +0530

    Update SignIn.jsx

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -35,10 +35,6 @@ function SignIn() {
               <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
               <input type="password" className="form-control" id="exampleInputPassword1" value={password} onChange={(e)=>setPassword(e.target.value)}/>
             </div>
-            <div className="mb-3 form-check">
-              <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-              <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-            </div>
             <button type="submit" className="btn btn-dark bg-dark">SignIn</button>
           </form>
         </div>
